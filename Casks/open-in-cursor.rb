e765d9b59d9094978e0f90cc32d66a197418e07a
Ivan Nemytchenko <nemytchenko@gmail.com>
2025-11-02 15:42:03 +0100

    Update version to 1.0.1

--- Casks/open-in-cursor.rb
+++ Casks/open-in-cursor.rb
@@ -1,5 +1,5 @@
 cask "open-in-cursor" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/inem/OpenInCursor/releases/download/v#{version}/Open-in-Cursor.app.zip"
